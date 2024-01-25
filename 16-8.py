bbb7ab705830b1a300a6865293b5654ac65db69a
Artur <fhnehbcvfubkjd@yandex.ru>
2024-01-25 15:37:12 +0500

    16-8

---
+++ 16-8.py
@@ -0,0 +1,17 @@
+old_messages = []
+
+
+def print_only_new(message):
+    if message not in old_messages:
+        print(message)
+    old_messages.append(message)
+
+print_only_new('Шутка номер 15')
+print_only_new('Шутка номер 23')
+print_only_new('Шутка номер 24')
+print_only_new('Шутка номер 24')
+print_only_new('Шутка номер 100')
+print_only_new('Шутка номер 24')
+print_only_new('Шутка номер 99')
+print_only_new('Шутка номер 15')
+print_only_new('Шутка номер 100')
